13fc2bce7959f4cdad52fe093acc2fed6a3d72c9
Firoj Hangarge <fhangarge@gmail.com>
2020-02-14 11:32:23 +0530

    Initial commit

---
+++ mysite/myproject/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    image = models.ImageField(upload_to='image/')
+    summary = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
